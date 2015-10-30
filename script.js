09d53fb68e15ca427f55f23b3e4ada580571234f
Richard Astbury <richard.astbury@gmail.com>
2015-10-30 15:44:16 +0000

    converted to leaflet, update to geojson

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-var map = new OpenLayers.Map("mapdiv");
-map.addLayer(new OpenLayers.Layer.OSM());
-var zoom=16;
-var markers = new OpenLayers.Layer.Markers( "Markers" );
-var center = new OpenLayers.LonLat( 1.1456667 , 52.041458000000006 ).transform(
-  new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-  map.getProjectionObject() // to Spherical Mercator Projection
-);
-map.addLayer(markers);
-map.setCenter(center, zoom);
+var map = L.map('map').setView([52.041458, 1.1456667], 13);
 
-var addMarker = function(lon, lat){
-  var lonLat = new OpenLayers.LonLat( lon, lat ).transform(
-    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-    map.getProjectionObject() // to Spherical Mercator Projection
-  );
-  var marker = new OpenLayers.Marker(lonLat);
-  markers.addMarker(marker);
-  return marker;
-};
-
-var hideImage = function(){
-  $('#image').hide();
-  $(window).off('click', hideImage);
-};
-
-var showImage = function(src){
-  $('#image').attr('src', src).show();
-  $(window).on('click', hideImage);
-};
-
-var makeSpot = function(spot){
-  var marker = addMarker(spot.lon, spot.lat);
-  marker.events.register("click", marker, function(){
-    showImage('http://i.imgur.com/rIZ7SUy.jpg');
-  });
-};
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 $.getJSON('spots.json', function(spots){
-  spots.forEach(makeSpot);
+  var onEachFeature = function(feature, layer) {
+
+    var description = (feature.properties.description || '').split(' ').filter(function(x){ return x.trim() !== ''}).map(function(x){
+      if (0 === x.indexOf('http://')) return '<a href="' + x + '">image</a>';
+      return x;
+    });
+    var content = '<strong>' + feature.properties.name + '</strong><p>' + description.join(' ') + '</p>';
+    
+    layer.bindPopup(content);
+  }
+  L.geoJson(spots, { onEachFeature: onEachFeature }).addTo(map);
 });
